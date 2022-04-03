a396511ed25d6ab084dba66ef571998a9c92bce1
ZaK3939 <tsu.aria.kalei@gmail.com>
2022-04-03 10:49:07 +0900

    fix some files

--- scripts/deployer/deploy-fakeloot.ts
+++ scripts/deployer/deploy-fakeloot.ts
@@ -2,24 +2,13 @@ const hre = require("hardhat");
 
 deployFakeLoot().catch((err) => console.log(err));
 
-const GOERLI_L1_STARKNET_ADDRESS = "0xde29d060D45901Fb19ED6C6e959EB22d8626708e";
 
 export async function deployFakeLoot() {
-  // const FakeLoot = await hre.ethers.getContractFactory("FakeLoot");
-  // const fakeloot = await FakeLoot.deploy();
+  const FakeLoot = await hre.ethers.getContractFactory("FakeLoot");
+  const fakeloot = await FakeLoot.deploy();
 
-  // await fakeloot.deployed();
+  await fakeloot.deployed();
 
-  // console.log("Loot deployed to:", fakeloot.address);
-  // console.log(`npx hardhat verify --network goerli ${fakeloot.address}`)
-
-  const L1L2Example = await hre.ethers.getContractFactory("L1L2Example");
-  const faketest = await L1L2Example.deploy(GOERLI_L1_STARKNET_ADDRESS);
-
-  await faketest.deployed();
-
-  console.log("test deployed to:", faketest.address);
-  console.log(
-    `npx hardhat verify --network goerli ${faketest.address} ${GOERLI_L1_STARKNET_ADDRESS}`
-  );
+  console.log("Loot deployed to:", fakeloot.address);
+  console.log(`npx hardhat verify --network goerli ${fakeloot.address}`)
 }
