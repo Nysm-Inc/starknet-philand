b517ac418b5ea208d45a39812e4da9a36f8b2ca9
ZaK3939 <tsu.aria.kalei@gmail.com>
2022-04-03 15:54:53 +0900

    refactor somefiles

--- contracts/l1/phi/MessageENS.sol
+++ contracts/l1/phi/MessageENS.sol
@@ -6,7 +6,10 @@ import { IStarkNetLike } from '../interfaces/IStarkNetLike.sol';
 import { MultiOwner } from '../utils/MultiOwner.sol';
 import '../utils/Strings.sol';
 
-
+/// @title Philand Contaract
+/// @author Zak3939.eth
+/// @notice create philand and object
+/// @dev check ENS (create philand) and check admin (mint Object NFT)
 contract MessageENS is MultiOwner{
 
     IStarkNetLike _starknetLike;
@@ -15,17 +18,20 @@ contract MessageENS is MultiOwner{
     address private _ensaddress;    
     address private _starknet;
 
-    mapping (string => address) public owner_lists;
-    mapping (string => uint256) public coupon_type;
-
-    bytes32 private baseNode = 0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae;
-    bytes32 public label;
-    
     uint256 private CREATE_PHILAND_SELECTOR =
         617099311689109934115201364618365113888900634692095419483864089403220532029;
-
     uint256 private CLAIM_L2_OBJECT_SELECTOR =
     725729645710710348624275617047258825327720453914706103365608274738200251740;
+
+    bytes32 public label;
+
+    //@notice baseNode = eth
+    bytes32 private baseNode = 0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae;
+    
+    //@notice for owner
+    mapping (string => address) public owner_lists;
+    //@notice for object claim
+    mapping (string => uint256) public coupon_type;
 
     error InvalidENS (address sender, string name,uint256 ensname_low,uint256 ensname_high ,bytes32 label,address owner, string node);
     error AllreadyClaimedPhiland (address sender, address owner,string name );
@@ -34,6 +40,7 @@ contract MessageENS is MultiOwner{
     event LogClaimL1NFT(string name,uint256 contract_address,uint256 tokenid);
     event LogClaimL2Object(string name,uint256 l2user_address, uint256 tokenid);
     
+    //@notice the coupon sent was signed by the admin signer
     struct Coupon {
     bytes32 r;
     bytes32 s;
@@ -50,7 +57,9 @@ contract MessageENS is MultiOwner{
         
     }
 
-
+    /**
+      Set l2 contract selector.
+    */
     function setCreatePhilandSelector(uint256 _createPhilandSelector) external onlyOwner {
         CREATE_PHILAND_SELECTOR = _createPhilandSelector;
     }
@@ -59,10 +68,20 @@ contract MessageENS is MultiOwner{
         CLAIM_L2_OBJECT_SELECTOR = _claimL2ObjectSelector;
     }
 
+    /**
+      Set ENS baseNode default is .eth
+    */
     function setEnsBaseNode(bytes32 _basenode) external onlyOwner {
         baseNode = _basenode;
     }
 
+    /*
+    * @title createPhiland
+    * @notice Send philand create Message from L1 to Starknet
+    * @param l2ContractAddress : starknet l2 phi contract
+    * @param name : ENS name
+    * @dev include check ENS 
+    */
     function createPhiland(
         uint256 l2ContractAddress,
         string calldata name
@@ -75,7 +94,7 @@ contract MessageENS is MultiOwner{
         uint256 ensname_high;
         (ensname_low,ensname_high)=toSplitUint(ensname);
         
-
+         // Check whether the user is ens owner or not
         if (msg.sender != _ens.owner(label)){
             revert InvalidENS({
                 sender: msg.sender,
@@ -106,6 +125,17 @@ contract MessageENS is MultiOwner{
         IStarkNetLike(_starknet).sendMessageToL2(l2ContractAddress, CREATE_PHILAND_SELECTOR, payload);
     }
 
+    /*
+    * @title claimObject
+    * @notice Send object create Message from L1 to Starknet
+    * @param l2ContractAddress : starknet l2 phi contract
+    * @param name : ENS name
+    * @param l2UserAddress : starknet account address
+    * @param tokenid : object nft token_id
+    * @param condition : object related name. ex.uniswap,loot,ethbalance,...
+    * @param coupon : get offchain api 
+    * @dev check that the coupon sent was signed by the admin signer
+    */
     function claimL2Object(
         uint256 l2ContractAddress,
         string calldata name,
@@ -119,6 +149,7 @@ contract MessageENS is MultiOwner{
         abi.encode(coupon_type[condition], msg.sender)
         ); 
     
+        // Check that the coupon sent was signed by the admin signer
         require(
         _isVerifiedCoupon(digest, coupon), 
         'Invalid coupon'
@@ -141,6 +172,7 @@ contract MessageENS is MultiOwner{
         IStarkNetLike(_starknet).sendMessageToL2(l2ContractAddress, CLAIM_L2_OBJECT_SELECTOR, payload);
     }
 
+    /// @dev check that the user has already claimed Philand
     function OwnerOfPhiland(string memory name) external view returns (bool){
         if (owner_lists[name]!=address(0))
             return true;
@@ -159,21 +191,24 @@ contract MessageENS is MultiOwner{
 		return signer == _adminSigner;
 	}
 
-
+    /// @dev for massaging to Starknet
     function toSplitUint(uint256 value) internal pure returns (uint256, uint256) {
     uint256 low = value & ((1 << 128) - 1);
     uint256 high = value >> 128;
     return (low, high);
     }
 
+    /// @dev get object conditon and number (related with offcahin validation)
     function getCouponType(string calldata condition) view public returns (uint256){
         return coupon_type[condition];
     }
 
+    /// @dev set object conditon and number (related with offcahin validation)
     function setCouponType(string calldata condition,uint256 tokenid) public onlyOwner() {
         coupon_type[condition] = tokenid;
     }
 
+    /// @dev For ENS subDomain
     function createENSLable(string calldata name) private returns (bytes32) {
         strings.slice memory slicee = strings.toSlice(name);
         strings.slice memory delim = strings.toSlice(".");
